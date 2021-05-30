7fab18b4dcaf8ba17b0ccca5532f6737e11ffecb
Dgoodwin92 <danielgoodwin85@gmail.com>
2021-05-30 15:12:07 +0000

    Update task into database

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def register():
         # check if username already exists in db
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
-        
+
         if existing_user:
             flash("Username already exists")
             return redirect(url_for("register"))
@@ -43,7 +43,7 @@ def register():
         mongo.db.users.insert_one(register)
 
         # put the new user into 'session' cookie
-        session["ueser"] = request.form.get("username").lower()
+        session["user"] = request.form.get("username").lower()
         flash("Registration Succesful!")
         return redirect(url_for("profile", username=session["user"]))
 
@@ -121,10 +121,22 @@ def add_task():
 
 @app.route("/edit_task/<task_id>", methods=["GET", "POST"])
 def edit_task(task_id):
-    task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    if request.method == "POST":
+        is_urgent = "on" if request.form.get("is_urgent") else "off"
+        submit = {
+            "category_name": request.form.get("category_name"),
+            "task_name": request.form.get("task_name"),
+            "task_description": request.form.get("task_description"),
+            "is_urgent": is_urgent,
+            "due_date": request.form.get("due_date"),
+            "created_by": session["user"]
+        }
+        mongo.db.tasks.update({"_id": ObjectId(task_id)}, submit)
+        flash("Task Succesfully Updated")
 
+    task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
     categories = mongo.db.categories.find().sort("category_name", 1)
-    return render_template("edit_task.html",task=task, categories=categories)
+    return render_template("edit_task.html", task=task, categories=categories)
 
 
 if __name__ == "__main__":
